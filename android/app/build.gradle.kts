c74d6903fd2ad889fb7a38469e7ade2ab6651d19
Inrego <renscottsim@gmail.com>
2026-02-16 20:18:28 +0100

    Different package name and app name for release and build.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,12 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+            manifestPlaceholders["appLabel"] = "BabyMonitarr Dev"
+        }
         release {
+            manifestPlaceholders["appLabel"] = "BabyMonitarr"
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
